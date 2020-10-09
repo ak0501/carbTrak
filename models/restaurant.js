417f81fac9b11fff4bf773b42589038a0472d930
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-10-09 13:10:57 -0500

    clean up

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,11 +1,9 @@
-
 module.exports = function(sequelize, DataTypes){
 var Restaurant = sequelize.define("Restaurant",{   
     name:{
         type:DataTypes.STRING,
         allowNull:false}
 });
-
 Restaurant.associate =function(models){
     Restaurant.hasMany(models.Food,{
         onDelete:"cascade"
